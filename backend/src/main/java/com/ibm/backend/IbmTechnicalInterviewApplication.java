36f47db79c648cdac53685789c934d32c35893a3
Jose Fajardo <jmfajardorst@gmail.com>
2024-06-08 23:54:46 -0500

    chore: Create application backend environment

---
+++ backend/src/main/java/com/ibm/backend/IbmTechnicalInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmTechnicalInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmTechnicalInterviewApplication.class, args);
+	}
+
+}
